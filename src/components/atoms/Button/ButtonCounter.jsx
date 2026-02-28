296c1a46c0dd66dee55766b6b23f9189ab7c6c00
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-02-28 16:23:45 +0100

    Add counter function to ButtonCounter

--- src/components/atoms/Button/ButtonCounter.jsx
+++ src/components/atoms/Button/ButtonCounter.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import './_Button.scss';
 import '../../../sass/abstract/_utils.scss';
 
@@ -5,12 +7,24 @@ import IconAddCount from '../Icon/IconAddCount.jsx';
 import IconRemoveCount from '../Icon/IconRemoveCount.jsx';
 
 export default function ButtonCounter() {
+    const [count, setCount] = useState(1);
+    const MAX_LIMIT = 99;
+    const MIN_LIMIT = 0;
+
+    const increment = () => {
+        setCount(count => (count < MAX_LIMIT ? count + 1 : count));
+    }
+
+    const decrement = () => {
+        setCount(count => (count > MIN_LIMIT ? count - 1 : count));
+    }
+
     return (
         <button className='button  button--cta_counter  button--bg_red' type='button'>
             <div className='button__container  button__container--space_between'>
-                <IconRemoveCount />
-                <span className='text_white_color  text_preset_4--bold'>1</span>
-                <IconAddCount />
+                <IconRemoveCount onClick={decrement} />
+                <span className='text_white_color  text_preset_4--bold'>{count}</span>
+                <IconAddCount onClick={increment} disabled={count >= MAX_LIMIT} />
             </div>
         </button>
     )
