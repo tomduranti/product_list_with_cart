b835713b20a26d85f32de95321441d026d7db0df
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-03-01 14:35:34 +0100

    Adjust GridItem size

--- src/components/molecules/GridItem/GridItem.jsx
+++ src/components/molecules/GridItem/GridItem.jsx
@@ -5,12 +5,14 @@ import ButtonAddToCart from '../../atoms/Button/ButtonAddToCart.jsx';
 export default function GridItem({ desktopSrc, tabletSrc, mobileSrc, alt, name, description, price }) {
     return (
         <div className="grid_item">
-            <picture className="grid_item__item">
-                <source srcSet={desktopSrc} media='(min-width: 90rem)' />
-                <source srcSet={tabletSrc} media='(min-width: 768px)' />
-                <img className="grid_item__image" src={mobileSrc} alt={alt} />
-                <ButtonAddToCart />
-            </picture>
+            <div className="grid_item__cta_image">
+                <picture className="grid_item__picture">
+                    <source srcSet={desktopSrc} media='(min-width: 90rem)' />
+                    <source srcSet={tabletSrc} media='(min-width: 768px)' />
+                    <img className="grid_item__image" src={mobileSrc} alt={alt} />
+                    <ButtonAddToCart />
+                </picture>
+            </div>
             <div className='grid_item__info'>
                 <span className='grid_item__name text_preset_4'>{name}</span>
                 <h3 className='grid_item__description text_preset_3'>{description}</h3>
