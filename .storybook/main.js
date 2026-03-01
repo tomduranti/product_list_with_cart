e91f60146b5e4276e505fcf81c102cb47b6ace41
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-03-01 20:34:16 +0100

    Add Desserts

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,7 @@ const config = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
-    '@storybook/addon-interactions',
-  ],
+  "addons": [],
   "framework": "@storybook/react-vite"
 };
 export default config;
---
+++ src/components/organisms/Desserts/Desserts.stories.jsx
@@ -0,0 +1,12 @@
+import Desserts from './Desserts';
+
+export default {
+    title: 'components/organisms/Desserts',
+    component: Desserts,
+}
+
+export const Default = {
+    render: () => (
+        <Desserts />
+    )
+}
