daa19c139f0c5eb4121fa777c7f9a005b8ac3b9b
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-03-01 21:00:17 +0100

    Complete static mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 
 import './sass/main.scss';
- 
-import GridItem from './components/molecules/GridItem/GridItem.jsx';
+import './sass/base/_base.scss';
+
+import Desserts from './components/organisms/Desserts/Desserts.jsx';
+import Cart from './components/atoms/Cart/Cart.jsx';
 
 export default function App() {
   return (
-    <>
-    </>
+    <div className='page'>
+      <div className='page__container'>
+        <Desserts />
+        <Cart />
+      </div>
+    </div>
   )
 }
