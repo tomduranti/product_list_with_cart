ed6d275a60fee6fa5395a5a8c1a4760e9790cb77
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-03-18 21:31:44 +0100

    Updates

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [],
+  "framework": "@storybook/react-vite"
+};
+export default config;
---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/react-vite').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
+
+import '../src/sass/abstract/_reset.scss';
+import '../src/sass/base/_typography.scss';
